e28476fa3e41ceb269d5ad7a2064c9920f2c3d4e
vicenteacosta3 <vicenteacosta3@f4c437aa-bde0-4334-bf84-7c7fc2a20a5e>
2011-10-28 01:39:57 +0000

    Varias cosas para traer datos de usuario de facebook. Falta guardar en la base

---
+++ trunk/FaceMovieApplication/FaceMovieApplication/Datatypes/DataFacebookUser.cs
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DataFacebookUser.cs" company="Interpool">
+//     Copyright Interpool. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace FaceMovieApplication.Datatypes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using FaceMovieApplication.FacebookCommunication;
+
+    /// <summary>
+    /// Class statement DataFacebookUser
+    /// </summary>
+    public class DataFacebookUser
+    {
+        /// <summary>
+        /// Gets or sets for Method.</summary>
+        public string FacebookUserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets for Method.</summary>
+        public OAuthFacebook OAuth { get; set; }
+
+        /// <summary>
+        /// Gets or sets for Method.</summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets for Method.</summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets for Method.</summary>
+        public string Gender { get; set; }
+        
+        /// <summary>
+        /// Gets or sets for Method.</summary>
+        public string PictureLink { get; set; }
+
+    }
+}
---
+++ trunk/FaceMovieApplication/FaceMovieApplication/Management/TestPage.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FaceMovieApplication.FacebookCommunication;
+
+namespace FaceMovieApplication.Management
+{
+    public partial class TestPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// test login
+        /// </summary>
+        /// <param name="sender">Parameter description for sender goes here</param>
+        /// <param name="e">Parameter description for e goes here</param>
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            OAuthFacebook oauth = new OAuthFacebook();
+            Response.Redirect(oauth.AuthorizationLinkGet());
+        }
+    }
+}
