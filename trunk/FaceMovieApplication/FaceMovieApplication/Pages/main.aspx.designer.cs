9a9ba8e5390c39662870e8d71b26878c5397d76e
JCGhiri@gmail.com <JCGhiri@gmail.com@f4c437aa-bde0-4334-bf84-7c7fc2a20a5e>
2011-11-26 19:56:15 +0000

    Modificada la main.asp para que incluya el sistema de rating. Todavía no está enganchado con la funcionalidad, pero ya se puede consultar el ranking.
    Hay que incluir el paquete Ajax toolkit, con nuget es más fácil.

--- trunk/FaceMovieApplication/FaceMovieApplication/Pages/main.aspx.designer.cs
+++ trunk/FaceMovieApplication/FaceMovieApplication/Pages/main.aspx.designer.cs
@@ -47,5 +47,41 @@ namespace FaceMovieApplication {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DataGrid Grid;
+        
+        /// <summary>
+        /// asm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager asm;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Submit1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputSubmit Submit1;
+        
+        /// <summary>
+        /// MovieRating control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.Rating MovieRating;
     }
 }
